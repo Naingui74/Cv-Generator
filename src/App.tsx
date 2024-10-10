6d78104a4e48193eebb5d7b3dca5e6cde8c64f0b
Naingui74 <aaron.groux@epitech.eu>
2024-10-10 18:08:52 -0400

    Add initial project files including font license, styles, and React components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import CandidateForm from './components/CandidateForm';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <CandidateForm />
+    </div>
+  );
+}
+
+export default App;
