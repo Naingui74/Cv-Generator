d37db55e254eafb5eb09a2f5e43b697c556a71f2
Naingui74 <aaron.groux@epitech.eu>
2024-10-10 20:22:00 -0400

    Remove unused imports and comments from EducationForm component

--- src/components/EducationForm.tsx
+++ src/components/EducationForm.tsx
@@ -1,7 +1,2 @@
-import React from 'react';
 import './styles/EducationForm.scss';
 import './styles/CandidateForm.scss';
-
-
-
-/*Ce composant gère la section de formation du candidat. Il permet d'ajouter des informations telles que le nom de l'école, le diplôme, le domaine d'étude, les dates de début et de fin, et une description. Il permet également d'ajouter plusieurs blocs de formation via un bouton.*/
